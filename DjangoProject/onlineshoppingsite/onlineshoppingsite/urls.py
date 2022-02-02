5eab6256e8031faacbdb4e77999ebd7677fc1386
Aniket S <41967375+anikets160@users.noreply.github.com>
2022-02-02 15:01:04 +0530

    Update urls.py

--- DjangoProject/onlineshoppingsite/onlineshoppingsite/urls.py
+++ DjangoProject/onlineshoppingsite/onlineshoppingsite/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('contactupdate/', contactupdate.site.urls),
+    path('contactupdateagain/', contactupdateagain.site.urls),
 ]
