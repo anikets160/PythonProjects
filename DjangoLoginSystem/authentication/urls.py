30d7838d03fdbf6434749b47e0a37ff77b98872f
Aniket S <anikets160@gmail.com>
2022-02-22 16:39:01 +0530

    Commit 16.38

--- DjangoLoginSystem/authentication/urls.py
+++ DjangoLoginSystem/authentication/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 from .import views
 
 urlpatterns = [
-    path('', views.home, name ="home"),
+    path('home', views.home, name ='home'),
+    path('', views.login, name ='login'),
     path('signup', views.signup, name ="signup"),
     path('signin', views.signin, name ="signin"),
     path('signout', views.signout, name ="signout"),
--- DjangoLoginSystem/authentication/views.py
+++ DjangoLoginSystem/authentication/views.py
@@ -5,13 +5,42 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, "authentication/index.html")
+    if 'login_status' in request.cookies and 'username' in request.cookies:
+        context =   {
+            'username' : request.cookies['username'],
+            'login_status' : request.cookies.get('logged_in'),
+        }
+        return render(request, 'home.html',context)
+    else:
+        return render(request, 'home.html')
+
+    # return render(request, 'home.html')
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+
+    if request.method == 'POST':
+        username = request.POST.get('email')
+
+    response = render(request, 'home.html')
+
+    # Setting Cookies
+    response.set_cookie('username', username)
+    response.set_cookie('login_status', True)
+    return  response
 
 def signup(request):
     return render(request, "authentication/signup.html")
 
+
 def signin(request):
-    return render(request, "authentication/signin.html")        
+    return render(request, "authentication/signin.html")
+
 
 def signout(request):
-    pass     
+    pass
+
+
+def logout(request):
+    pass
